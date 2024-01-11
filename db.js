6a0a33169834aff7aed77c712c634a510a4f9f79
Narue-GS <naruegit@gmail.com>
2024-01-11 11:24:30 -0300

    feat: add pg package and create fake client

--- db.js
+++ db.js
@@ -14,3 +14,17 @@ const nodes = [
 ]
 
   module.exports = {nodes}
+
+  import { Client } from 'pg'
+  const client = new Client({
+    host: 'my.database-server.com',
+    port: 5334,
+    database: 'database-name',
+    user: 'database-user',
+    password: 'secretpassword!!',
+  })
+  await client.connect()
+   
+  const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+  console.log(res.rows[0].message) // Hello world!
+  await client.end()
